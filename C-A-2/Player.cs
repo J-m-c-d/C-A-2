30ed0fb5f86b5c32ae82270065a671b5abf18163
J-m-c-d <123364114+J-m-c-d@users.noreply.github.com>
2023-12-14 18:56:58 +0000

    Player And Team Started

--- C-A-2/Player.cs
+++ C-A-2/Player.cs
@@ -8,5 +8,20 @@ namespace C_A_2
 {
     internal class Player
     {
+        //set up vars
+        public string Name { get; set; }
+        public string ResultRecord { get; set; }
+
+        public int score { get; set; }
+
+        //constructors
+
+
+        public override string ToString()
+        {
+            //change the names of teams to strings
+            return $"{Name.ToString()}-{ResultRecord.ToString()}-{score}";
+
+        }
     }
 }
